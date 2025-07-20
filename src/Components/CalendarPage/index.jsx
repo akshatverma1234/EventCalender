3c2151e1b7c11564c1706746985d38397481f12e
Akshat Verma <vakshat613@gmail.com>
2025-07-20 12:06:46 +0530

    Added the event functions

--- src/Components/CalendarPage/index.jsx
+++ src/Components/CalendarPage/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { use, useState } from "react";
 import { FaAngleRight, FaAngleLeft } from "react-icons/fa6";
 import { IoClose } from "react-icons/io5";
 import { LuPencilLine } from "react-icons/lu";
@@ -23,6 +23,11 @@ const CalenderPage = () => {
   const currentDate = new Date();
   const [currentMonth, setCurrentMonth] = useState(currentDate.getMonth());
   const [currentYear, setCurrentYear] = useState(currentDate.getFullYear());
+  const [selectDate, setSelectedDate] = useState(currentDate);
+  const [showEvent, setShowEvent] = useState(false);
+  const [events, setEvents] = useState([]);
+  const [eventTime, setEventTime] = useState({ hours: "00", minutes: "00" });
+  const [eventText, setEventText] = useState(" ");
 
   const prevMonth = () => {
     setCurrentMonth((prevMonth) => (prevMonth === 0 ? 11 : prevMonth - 1));
@@ -38,6 +43,39 @@ const CalenderPage = () => {
     );
   };
 
+  const isSameDay = (day1, day2) => {
+    return (
+      day1.getFullYear() === day2.getFullYear() &&
+      day1.getMonth() === day2.getMonth() &&
+      day1.getDate() === day2.getDate()
+    );
+  };
+  const handleClick = (day) => {
+    const clickedDate = new Date(currentYear, currentMonth, day);
+    const today = new Date();
+
+    if (clickedDate >= today || isSameDay(clickedDate, today)) {
+      setSelectedDate(clickedDate);
+      setShowEvent(true);
+      setEventTime({ hours: "00", minutes: "00" });
+      setEventText("");
+    }
+  };
+
+  const handleEventSubmit = () => {
+    const newEvent = {
+      date: selectDate,
+      time: `${eventTime.hours.padStart(2, "0")}:${eventTime.minutes.padStart(
+        2,
+        "0"
+      )}`,
+      text: eventText,
+    };
+    setEvents([...events, newEvent]);
+    setEventTime({ hours: "00", minutes: "00" });
+    setEventText("");
+    setEvents(false);
+  };
   const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
   const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
 
@@ -81,6 +119,7 @@ const CalenderPage = () => {
                   ? "currentDate"
                   : ""
               }
+              onClick={() => handleClick(day + 1)}
             >
               {day + 1}
             </span>
@@ -89,32 +128,53 @@ const CalenderPage = () => {
       </div>
 
       <div className="events w-[60%] h-[100%] !py-12">
-        <div className="event-popup absolute top-[38%] left-[3rem] bg-black aspect-[10/9] rounded-[1rem] shadow-2xl w-[clamp(25rem,21cqi,40rem)] flex flex-col justify-center items-center gap-y-8 hidden">
-          <div className="time-input flex gap-y-[1rem] text-white">
-            <div className="event-popup-time">Time</div>
-            <input
-              type="number"
-              name="hours"
-              min={0}
-              max={24}
-              className="hours"
-              placeholder="HH"
-            />
-            <input
-              type="number"
-              name="minutes"
-              min={0}
-              max={60}
-              className="minutes"
-              placeholder="MM"
-            />
+        {showEvent && (
+          <div className="event-popup absolute top-[38%] left-[3rem] bg-black aspect-[10/9] rounded-[1rem] shadow-2xl w-[clamp(25rem,21cqi,40rem)] flex flex-col justify-center items-center gap-y-8">
+            <div className="time-input flex gap-y-[1rem] text-white">
+              <div className="event-popup-time">Time</div>
+              <input
+                type="number"
+                name="hours"
+                min={0}
+                max={24}
+                className="hours"
+                placeholder="HH"
+                value={eventTime.hours}
+                onChange={(e) =>
+                  setEventTime({ ...eventTime, hours: e.target.value })
+                }
+              />
+              <input
+                type="number"
+                name="minutes"
+                min={0}
+                max={60}
+                className="minutes"
+                placeholder="MM"
+                value={eventTime.minutes}
+                onChange={(e) =>
+                  setEventTime({ ...eventTime, minutes: e.target.value })
+                }
+              />
+            </div>
+            <textarea
+              placeholder="Enter Event Text (Maximum 60 Characters)"
+              value={eventText}
+              onChange={(e) => {
+                if (e.target.value.length <= 60) {
+                  setEventText(e.target.value);
+                }
+              }}
+            ></textarea>
+            <button className="event-popup-btn">Add Event</button>
+            <button
+              className="close-event-popup"
+              onClick={() => setShowEvent(false)}
+            >
+              <IoClose className="text-white text-[1.5rem]" />
+            </button>
           </div>
-          <textarea placeholder="Enter Event Text (Maximum 60 Characters)"></textarea>
-          <button className="event-popup-btn">Add Event</button>
-          <button className="close-event-popup">
-            <IoClose className="text-white text-[1.5rem]" />
-          </button>
-        </div>
+        )}
 
         <div className="event">
           <div className="event-date-wrapper">
